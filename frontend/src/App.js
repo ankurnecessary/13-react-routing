a024fc06adca8ccaafe829dcf5e081220871db40
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-09 03:51:02 +0930

    Resolved ESLint related issues from App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,8 +19,9 @@
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+import React from 'react';
 
-function App() {
+function App () {
   return <div></div>;
 }
 
