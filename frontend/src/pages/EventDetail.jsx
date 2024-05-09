d2ff2c71ba2851dcca7a98fe61b5b4eb8a3d36b0
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-10 04:19:12 +0930

    Showing event id on event detail page.

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function EventDetailPage () {
-  return <h1>Event Detail</h1>;
+  const params = useParams();
+  return (
+    <>
+      <h1>Event Detail</h1>
+      <p>{params.id}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
