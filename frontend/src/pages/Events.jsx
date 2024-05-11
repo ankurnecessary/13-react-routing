cfdea1e182e80b7103b66a68dbd51b301d166006
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-12 06:26:54 +0930

    react-router: When an Error{} object is thrown via loader function, it loads nearest most errorElement component to it.

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -22,7 +22,7 @@ export default EventsPage;
 export async function loader () {
   const response = await fetch('http://localhost:8080/events1');
   if (!response.ok) {
-    return { isError: true, message: 'Could not fetch events.' };
+    throw new Error({ message: 'Could not fetch events.' });
   } else {
     // React router can handle Response{} object. So, we need not to extract data via response.json()
     return response;
