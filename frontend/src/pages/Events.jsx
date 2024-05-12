f84ff343deed93e05d5c4578ff9678e33b3ecf49
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-13 06:12:40 +0930

    [react-router]: Using react-router-dom's inbuilt json() function instead of Response{} object to throw errors from router loader() functions.

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage () {
@@ -23,9 +23,10 @@ export async function loader () {
   const response = await fetch('http://localhost:8080/events');
   if (!response.ok) {
     // return {isError: true, message: 'Could not fetch events.'}
-    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-      status: 500
-    });
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500
+    // });
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
     // React router can handle Response{} object. So, we need not to extract data via response.json()
     return response;
