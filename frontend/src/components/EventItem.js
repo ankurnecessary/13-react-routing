ffee9ca1c6ccd9092369c282dc35b5044367377b
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-16 05:16:42 +0930

    [react-router]: Using the router action function via useSubmit() hook instead of <Form />

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSubmit } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classes from './EventItem.module.css';
 
 function EventItem ({ event }) {
+  const submit = useSubmit();
   function startDeleteHandler () {
-    // ...
+    const proceed = window.confirm('Are your sure?');
+    if (proceed) {
+      // we can use 'action' here if we want to use the action function of some other route.
+      // Otherwise it will use the action function of same route
+      // null mentioned below is so because we are not sending any data to the action function
+      // If we want to send any data then we will replace null with that data
+
+      // submit(null, { method: 'DELETE', action: '/a-different-path' });
+      submit(null, { method: 'DELETE' });
+    }
   }
 
   return (
